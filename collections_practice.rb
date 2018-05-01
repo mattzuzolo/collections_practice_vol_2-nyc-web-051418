5bcb349213c0f203c554d63fdc6a753e8105b7e7
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-01 22:36:55 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
 # your code goes here
+
+def begins_with_r (passed_array)
+  
+  i = 0
+  still_true = true
+  
+  while passed_array.length > i
+  
+    if passed_array[i][0].downcase != "r"
+        return false
+    else
+        still_true = true
+    end
+     i += 1
+  end    
+  still_true
+end
+
+
+
+def contain_a (passed_array)
+  
+    passed_array.select { |elements| elements.include?("a") }
+ 
+end
+
+
+def first_wa (passed_array)
+  
+  passed_array.map {|elements| elements.to_s}
+  
+  
+  passed_array.find { |elements| elements.include?("wa") }
+  
+end
